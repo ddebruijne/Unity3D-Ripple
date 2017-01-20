1dc7d788b90ac37c25579f4f02d72c42cc09faa4
Danny de Bruijne <dannydb44@gmail.com>
2017-01-21 00:50:33 +0100

    Something to read score.

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -26,7 +26,7 @@ public class Note : MonoBehaviour {
 	}
 	
 	void Update () {
-		if ( move )  transform.position = new Vector3(transform.position.x - 0.1f, transform.position.y, 1);
+		if ( move )  transform.position = new Vector3(transform.position.x - 0.1f, transform.position.y, 0);
 	}
 
 	void SetNoteType(NoteType type) {
@@ -47,7 +47,7 @@ public class Note : MonoBehaviour {
 			break;
 			default:
 			break;
-
+		
 		}
 	}
 
