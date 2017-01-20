04ae61ad59cd4e3657b1a1c5714f1c8600c46964
Randy Schouten <randyschouten@outlook.com>
2017-01-20 20:31:02 +0100

    Added default project

---
+++ Assets/Scripts/WaveGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveGenerator : MonoBehaviour {
+
+    public int segments = 1;
+    private int vertsPerSegment = 4;
+
+
+    public Mesh mesh;
+
+    void Start() {
+        CreateMesh();
+    }
+
+	void Update () {
+        UpdateMesh();
+	}
+
+    public void CreateMesh() {
+        if (mesh != null) return;
+
+        mesh = new Mesh();
+        mesh.name = "Wave_Mesh";
+
+        UpdateMesh();
+    }
+
+    public void UpdateMesh() {
+        mesh.;
+
+
+    }
+}
