a585ea2844ef81ca43d55ec75c095b37302fb56a
Randy Schouten <randyschouten@outlook.com>
2017-01-21 11:56:11 +0100

    Added grid stuff

---
+++ Assets/Scripts/Grid/CubeGrid.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGrid : MonoBehaviour {
+
+    public List<GridCube> cubes = new List<GridCube>();
+    public float raiseRange = 2.5f;
+    public float maxRaiseHeight = 1;
+
+    void Update() {
+        ResetRaise();
+
+        SetRaiseAmount(new Vector2(Mathf.Sin(Time.time) * 4, 0), 2);
+        SetRaiseAmount(new Vector2(0, Mathf.Sin(Time.time) * 4), 2);
+    }
+
+    public void ResetRaise() {
+        foreach (GridCube cube in cubes) {
+            cube.SetRaiseAmount(0);
+        }
+    }
+
+    public void SetRaiseAmount(Vector2 location, float a) {
+        foreach(GridCube cube in cubes) {
+            float distance = raiseRange / Vector2.Distance(location, new Vector2(cube.transform.position.x, cube.transform.position.z));
+            float clampedDistance = Mathf.Clamp01(distance);
+            float amount = clampedDistance * a;
+            cube.AddRaiseAmount(amount);
+        }
+    }
+
+    [ContextMenu("Find Cubes")]
+    private void FindCubes() {
+        cubes = new List<GridCube>(gameObject.GetComponentsInChildren<GridCube>());
+    }
+}
---
+++ Assets/Scripts/Grid/GridCube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCube : MonoBehaviour {
+
+    private Vector3 defaultPos;
+
+    public float raiseAmount = 0;
+    private float actualRaiseAmount = 0;
+
+    void Start() {
+        defaultPos = new Vector3(transform.position.x, -4, transform.position.z);
+    }
+
+    public void SetRaiseAmount(float a) {
+        raiseAmount = a;
+    }
+
+    public void AddRaiseAmount(float a) {
+        raiseAmount += a;
+    }
+
+	void Update () {
+        actualRaiseAmount = Mathf.Lerp(actualRaiseAmount, raiseAmount, 0.25f);
+        transform.position = defaultPos + new Vector3(0, actualRaiseAmount, 0);
+	}
+}
