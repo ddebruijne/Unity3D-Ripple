f5d1fa81b877f4df4c99dc86f1398e5fdb1c2b1c
Danny de Bruijne <dannydb44@gmail.com>
2017-01-22 00:11:05 +0100

    Pulse the press A button

---
+++ Assets/Scripts/Effects/UIPulse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPulse : MonoBehaviour {
+
+	float amplitude = 0.0025f;
+	float period = 1f;
+	Vector3 startscale;
+
+	void Start() {
+		startscale = transform.localScale;
+	}
+
+	void FixedUpdate () {
+		float theta = Time.timeSinceLevelLoad / period;
+		float distance = amplitude * Mathf.Sin(theta);
+		transform.localScale = transform.localScale + new Vector3(1,1,0) * distance;
+	}
+}
