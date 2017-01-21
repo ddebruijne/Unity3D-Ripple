9968fc89fd15eef1e0dd235c12aef6afaafacea2
Randy Schouten <randyschouten@outlook.com>
2017-01-21 17:32:24 +0100

    Add player control

--- Assets/Scripts/Grid/CubeGrid.cs
+++ Assets/Scripts/Grid/CubeGrid.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CubeGrid : MonoBehaviour {
 
+    public static CubeGrid Instance;
+
     public List<GridCube> cubes = new List<GridCube>();
     public float raiseRange = 2.5f;
     public float maxRaiseHeight = 1;
@@ -11,16 +13,21 @@ public class CubeGrid : MonoBehaviour {
     public int playerCount = 2;
     public List<Color> playerColors = new List<Color>();
 
+    void Awake() {
+        Instance = this;
+    }
+
     void Update() {
-        SetRaiseAmount(new Vector2(Mathf.Sin(Time.time / 2) * 10, Mathf.PerlinNoise(Time.time, Time.time) + Mathf.Sin(Time.time / 4) * 5) - new Vector2(5,5), 2, 0);
-        SetRaiseAmount(new Vector2(Mathf.PerlinNoise(Time.time, Time.time) + Mathf.Sin(Time.time / 5) * 10, Mathf.Sin(Time.time / 2) * 10) - new Vector2(5, 5), 2, 1);
+        //SetRaiseAmount(new Vector2(Mathf.Sin(Time.time / 2) * 10, Mathf.PerlinNoise(Time.time, Time.time) + Mathf.Sin(Time.time / 4) * 5) - new Vector2(5,5), 2, 0);
+        //SetRaiseAmount(new Vector2(Mathf.PerlinNoise(Time.time, Time.time) + Mathf.Sin(Time.time / 5) * 10, Mathf.Sin(Time.time / 2) * 10) - new Vector2(5, 5), 1, 1);
     }
 
     public void SetRaiseAmount(Vector2 location, float a, int player) {
+        a = Mathf.Clamp01(a);
         foreach(GridCube cube in cubes) {
             float distance = raiseRange / Vector2.Distance(location, new Vector2(cube.transform.position.x, cube.transform.position.z));
             float clampedDistance = Mathf.Clamp01(distance);
-            float amount = clampedDistance * a;
+            float amount = clampedDistance * a * 3;
             cube.SetRaiseAmount(amount, player);
         }
     }
--- Assets/Scripts/PlayerBall.cs
+++ Assets/Scripts/PlayerBall.cs
@@ -5,9 +5,17 @@ using XboxCtrlrInput;
 
 public class PlayerBall : MonoBehaviour {
 
-	[ReadOnly]	public int playerIndex;
+    [ReadOnly]  public int playerIndex;
 	[ReadOnly]	public XboxController MappedController;
 	[ReadOnly]	public int score;
+
+    public Camera cam;
+    public Vector2 currentPos = Vector2.zero;
+    public Vector2 velocity = Vector2.zero;
+
+    void Start() {
+
+    }
 
 	public void SetupPlayer(int _playerIndex) {
 		playerIndex = _playerIndex;
@@ -27,8 +35,8 @@ public class PlayerBall : MonoBehaviour {
 		score = 0;
 	}
 
-	void Start () {
-		
+	void Awake () {
+        cam = Camera.main;
 	}
 	
 	void Update () {
@@ -36,8 +44,15 @@ public class PlayerBall : MonoBehaviour {
 	}
 
 	void ControllerInput() {
-		//Debug.Log("P" + playerIndex + ": " + XCI.GetAxis(XboxAxis.LeftStickX, MappedController));
-		
+        Vector3 moveDirection = new Vector3( XCI.GetAxis(XboxAxis.LeftStickX, MappedController), 0, XCI.GetAxis(XboxAxis.LeftStickY, MappedController));
+        moveDirection = cam.transform.TransformDirection(moveDirection);
+
+        velocity += new Vector2(moveDirection.x, moveDirection.z) * 2;
+        currentPos += velocity * Time.deltaTime;
+
+        CubeGrid.Instance.SetRaiseAmount(currentPos, XCI.GetButton(XboxButton.A, MappedController) ? 1 : 0.1f, playerIndex);
+
+        velocity /= 1.2f;
 	}
 
 	public void AddScore(int ammount) { score += ammount; }
