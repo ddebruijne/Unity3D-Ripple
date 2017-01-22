79dc3b71d5d761eb1aa019ef9eb25f38cfd45427
Danny de Bruijne <dannydebruijne@outlook.com>
2017-01-22 11:40:08 +0100

    Patch up the splash

---
+++ Assets/Scripts/Effects/UIRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIRotate : MonoBehaviour {
+
+	float speed = 0.0065f;
+
+	private void FixedUpdate() {
+		//transform.localRotation = transform.localRotation.z + new Vector3(0, 0, 1) * speed;
+		transform.RotateAroundLocal(new Vector3(0, 0, -1), speed);
+	}
+}
