2d7c9ded958c02c8427f90630ca963b4acb88c1b
Randy Schouten <randyschouten@outlook.com>
2017-01-22 14:09:01 +0100

    Added player starting points

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -12,7 +12,12 @@ public class Goal : MonoBehaviour {
 	public void SetupGoal(PlayerBall _player) {
 		player = _player;
 		gridColor = CubeGrid.Instance.playerColors[player.playerIndex];
-	}
+
+        Vector3 origin = GameManager.instance.transform.position;
+        Vector2 origin2D = new Vector2(origin.x, origin.z);
+        player.currentPos = origin2D + ((new Vector2(transform.position.x, transform.position.z) - origin2D).normalized * 4);
+
+    }
 
     void Update() {
         CubeGrid.Instance.SetRaiseAmount(new Vector2(goalPost.transform.position.x, goalPost.transform.position.z), Mathf.Sin(Time.time) + 1.5f, 1, 0, player.playerIndex + 6);
