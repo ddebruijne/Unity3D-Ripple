f8ea5a34c0a7261e426ed91d034ebf1c2d4f240a
Danny de Bruijne <dannydb44@gmail.com>
2017-02-26 16:01:43 +0100

    Add loading screen

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+	public int levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(CallLoad());
+	}
+
+	IEnumerator CallLoad() {
+		AsyncOperation async = SceneManager.LoadSceneAsync(levelToLoad, LoadSceneMode.Single);
+		yield return async;
+	}
+	
+
+}
